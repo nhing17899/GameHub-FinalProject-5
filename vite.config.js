56b328cb465cd3c7c1fd0eafedf01e063af77337
Nhi Nguyen <nhing17899@gmail.com>
2024-04-15 20:18:39 -0400

    Change base config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gamehub-5',
+  base: '/GameHub-FinalProject-5',
 })
